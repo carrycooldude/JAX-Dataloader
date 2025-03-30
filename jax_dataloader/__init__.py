a32850a078ceaac121ee8718edc6bd2fba2fcbeb
carrycooldude <rawatkari554@gmail.com>
2025-03-30 16:14:20 +0530

    Update __init__.py to expose all necessary classes and methods

--- jax_dataloader/__init__.py
+++ jax_dataloader/__init__.py
@@ -1,4 +1,36 @@
-from .jax_dataloader import JAXDataLoader, DataLoaderConfig, load_custom_data
+"""JAX DataLoader - A high-performance data loading library for JAX applications."""
 
-__version__ = '0.1.0'
-__all__ = ['JAXDataLoader', 'DataLoaderConfig', 'load_custom_data']
+from .jax_dataloader import DataLoader, DataLoaderConfig, load_custom_data
+from .data import BaseLoader, CSVLoader, JSONLoader, ImageLoader
+from .memory import MemoryManager, Cache
+from .progress import ProgressTracker
+from .transform import Transform
+from .exceptions import DataLoaderError, ConfigurationError, MemoryError
+from .utils import (
+    get_available_memory,
+    calculate_batch_size,
+    get_device_count,
+    format_size
+)
+
+__version__ = '0.1.7'
+__all__ = [
+    'DataLoader',
+    'DataLoaderConfig',
+    'load_custom_data',
+    'BaseLoader',
+    'CSVLoader',
+    'JSONLoader',
+    'ImageLoader',
+    'MemoryManager',
+    'Cache',
+    'ProgressTracker',
+    'Transform',
+    'DataLoaderError',
+    'ConfigurationError',
+    'MemoryError',
+    'get_available_memory',
+    'calculate_batch_size',
+    'get_device_count',
+    'format_size'
+]
